aae614b6338989e4401383390c54dc5f29334293
Tickether <tickether@gmail.com>
2023-03-03 21:54:38 +0000

    Update memberships.js
    file name error fix

--- routes/memberships.js
+++ routes/memberships.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createMembership, getMembership, getMemberships, updateMembership } from '../controllers/Membership.js';
+import { createMembership, getMembership, getMemberships, updateMembership } from '../controllers/membership.js';
 
 const router = express.Router();
 
